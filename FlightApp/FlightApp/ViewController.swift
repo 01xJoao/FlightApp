018c1725ce83a99ac4073728b6d4d8117f72e5de
João Palma <joaowd@outlook.com>
2020-07-30 20:18:54 +0100

    Create FlightApp Project

---
+++ FlightApp/FlightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightApp
+//
+//  Created by João Palma on 30/07/2020.
+//  Copyright © 2020 João Palma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
